eb611fec15a5a89867d8582183d7b5cd6aabaaa9
friday <54495836+fridayowl@users.noreply.github.com>
2023-02-25 02:01:53 +0530

    Create this one works.py

---
+++ this one works.py
@@ -0,0 +1,20 @@
+from scrapingbee import ScrapingBeeClient
+from bs4 import BeautifulSoup
+
+client = ScrapingBeeClient(api_key='UVDRFJ0V9G6L6XU0UW8PW3WDP1PQG7IIBMWG4XUA08Z30P0TKT7WPXGHU1OE6HGVJOQWSG41GU52O7G9')
+response = client.get('https://www.udemy.com/course/digital-marketing-with-google-my-business-seo-website-local-listing/?ranMID=39197&ranEAID=vWFcdslQDtg&ranSiteID=vWFcdslQDtg-H_wirk0hZgIihnSvFsx35Q&LSNPUBID=vWFcdslQDtg&utm_source=aff-campaign&utm_medium=udemyads&couponCode=230223')
+
+print('Response HTTP Status Code: ', response.status_code)
+print('Response HTTP Response Body: ', response.content)
+soup = BeautifulSoup(response.content, 'html.parser')
+# find all text in the HTML content
+discount_tags = soup.find_all(attrs={'class': lambda x: x and 'discount' in x})
+for tag in discount_tags:
+    print(f"Attribute: {tag.attrs}, Class: {tag['class']}")
+discount_tag = soup.find('div', class_='ud-clp-percent-discount')
+
+exp = soup.find('div', class_='generic-purchase-section--buy-box-main--2o6Au')
+print(exp)
+
+# Print the text inside the tag
+print(discount_tag)
